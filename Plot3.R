bf558d382325a3297ebaefc43ba586581dd5ac77
rafiamiruddin1 <rafi.amiruddin@gmail.com>
2015-08-10 01:36:26 +0500

    Create Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,8 @@
+> png(filename='plots/plot3.png',width=480,height=480,units='px')
+> lncol<-c('black','red','blue')
+> lbls<-c('Sub_metering_1','Sub_metering_2','Sub_metering_3')
+> plot(power.consumption$DateTime,power.consumption$SubMetering1,type='l',col=lncol[1],xlab='',ylab='Energy sub metering')
+> lines(power.consumption$DateTime,power.consumption$SubMetering2,col=lncol[2])
+> lines(power.consumption$DateTime,power.consumption$SubMetering3,col=lncol[3])
+> legend('topright',legend=lbls,col=lncol,lty='solid')
+> x<-dev.off()
